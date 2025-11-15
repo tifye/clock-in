83c706c2afb831047c95afbe148cdbe841e0cd1e
Joshua De Matas <joshua.de.matas@lendo.se>
2025-11-15 17:40:48 +0100

    init: cobra setup with noop root cmd

---
+++ cmd/root.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"context"
+	"os"
+
+	"github.com/charmbracelet/log"
+	"github.com/spf13/cobra"
+)
+
+func newRootCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "clock",
+		Run: func(cmd *cobra.Command, _ []string) {
+			cmd.Help()
+		},
+	}
+
+	return cmd
+}
+
+func Execute() {
+	logger := log.NewWithOptions(os.Stdout, log.Options{})
+	logger.Print("Meep")
+
+	rootCmd := newRootCommand()
+
+	err := rootCmd.ExecuteContext(context.TODO())
+	if err != nil {
+		logger.Error("Error executing command", "error", err)
+		os.Exit(1)
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"github.com/tifye/clock-in/cmd"
+)
 
 func main() {
-	fmt.Println("Meep")
+	cmd.Execute()
 }
